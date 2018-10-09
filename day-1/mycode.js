29c90f7056a96069112214ab03a50d13fcc7dbf7
Alexander Madsen <akmadsen522@gmail.com>
2018-10-09 09:48:17 -0600

    Finished exercises up to appending

--- day-1/mycode.js
+++ day-1/mycode.js
@@ -2,7 +2,38 @@
  * Here is my custom JS code.
  */
 
-// Custom code
+
+// EXAMPLE: Logging
+console.log("EXAMPLE: Logging");
 alert("Gotcha!");
 console.log("Hey there");
 document.write("1337 haxx0rz"); 
+
+
+// EXAMPLE: Vars 
+console.log("\nEXAMPLE: Vars")
+var test = "I am a Variable"; 
+console.log(test); 
+
+// EXAMPLE: Arithmetic 
+var num1 = 4; 
+var num2 = 3; 
+
+console.log("\nEXAMPLE: Arithmetic")
+console.log("num1: " + num1 + "\t\tnum2: " + num2); 
+console.log("Negation:\t\t" + (-num1)); 
+console.log("Addition:\t\t" + (num1 + num2)); 
+console.log("Subtraction:\t" + (num1 - num2)); 
+console.log("Multiplication:\t" + (num1 * num2)); 
+console.log("Division:\t\t" + (num1 / num2)); 
+console.log("Modulo:\t\t\t" + (num1 % num2)); 
+
+console.log("Div by 0:\t\t" + (1 / 0));
+console.log("sqrt -1:\t\t" + Math.sqrt(-1)); 
+
+// EXAMPLE: Your name 
+console.log("\nEXAMPLE: Concatenation"); 
+var firstName = "Alexander"; 
+var lastName = "Madsen"; 
+var fullName = firstName + " " + lastName; 
+console.log("Full Name:", fullName); // Testing comma
