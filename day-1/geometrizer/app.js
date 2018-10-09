a36c7367754e32d99c9d57433cdd33dcd6839c56
Alexander Madsen <akmadsen522@gmail.com>
2018-10-09 10:25:54 -0600

    Geometrizer finished

---
+++ day-1/geometrizer/app.js
@@ -0,0 +1,8 @@
+let radius = 2; 
+
+let circumference = 2 * Math.PI * radius; 
+let area = Math.PI * radius * radius; 
+
+console.log("radius:", radius); 
+console.log("The circumference is", circumference); 
+console.log("The area is", area); 
