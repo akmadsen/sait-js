74482927433ffc2b03ab4b07735c82ae7c00a688
Alexander Madsen <akmadsen522@gmail.com>
2018-10-09 08:48:31 -0600

    Separated out js code

---
+++ day-1/mycode.js
@@ -0,0 +1,12 @@
+/* 
+    Multiline
+    Comment 
+*/
+
+// Single Line Comment 
+alert("Hello, world!");
+console.log("Secret message"); // Inline Comment 
+
+alert("Gotcha!");
+console.log("Hey there");
+document.write("1337 haxx0rz"); 
