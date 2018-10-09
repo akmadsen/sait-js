d10689576c42a6bd9d4da31b4b3c9951faa6f915
Alexander Madsen <akmadsen522@gmail.com>
2018-10-09 10:18:08 -0600

    finished age calculator

---
+++ day-1/age-calculator/app.js
@@ -0,0 +1,8 @@
+let birthYear = 1989; 
+let futureYear = 2099; 
+
+let oldAge = futureYear - birthYear; 
+let youngAge = oldAge - 1; 
+
+document.write("<h1>Age Calculator</h1>"); 
+document.write("<p>I will be either " + youngAge + " or " + oldAge + " in " + futureYear + "</p>")
