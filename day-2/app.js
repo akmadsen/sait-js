4cd005a27b0661874dfffcfa65238cf79d4b6ed0
Alexander Madsen <akmadsen522@gmail.com>
2018-10-10 08:47:05 -0600

    For loop exercise complete

--- day-2/app.js
+++ day-2/app.js
@@ -1,5 +1,5 @@
 // EXAMPLE: Times Tables 
-console.log("EXAMPLE: Times Tables"); 
+console.log("EXAMPLE: While Times Tables"); 
 
 let number = 1; 
 while (number <= 12) { 
@@ -7,13 +7,23 @@ while (number <= 12) {
     number++; 
 }
 
-console.log("\n\n"); 
-number = 1; 
-while (number <= 12) { 
-    let counter = 1; 
-    while (counter <= 12) { 
-        console.log(counter + " x " + number + " = " + (counter * number)); 
-        counter++; 
-    }
-    number++; 
+// This is just a bit long, so disabled for now
+// console.log("\n\n"); 
+// number = 1; 
+// while (number <= 12) { 
+//     let counter = 1; 
+//     while (counter <= 12) { 
+//         console.log(counter + " x " + number + " = " + (counter * number)); 
+//         counter++; 
+//     }
+//     number++; 
+// }
+
+
+
+// EXAMPLE: For Loops
+console.log("\nEXAMPLE: For Times Tables"); 
+
+for (number = 1; number <= 12; number++) { 
+    console.log("9 x " + number + " = " + (9 * number)); 
 }
