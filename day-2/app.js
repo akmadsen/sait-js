806c72a1888f62dd3c325bbc890f4507ba87f81b
Alexander Madsen <akmadsen522@gmail.com>
2018-10-10 09:56:59 -0600

    array example done

--- day-2/app.js
+++ day-2/app.js
@@ -31,10 +31,11 @@ for (number = 1; number <= 12; number++) {
 // Also disabled for being kind of long output
 // console.log("\n\n")
 // for (number = 1; number <= 12; number++) { 
-//     for(counter = 1; counter <= 12; counter++) {
+//     for(let counter = 1; counter <= 12; counter++) {
 //         console.log(counter + " x " + number + " = " + (counter * number)); 
 //     }
 // }
+
 
 
 // EXAMPLE: Loop Logic
@@ -46,4 +47,67 @@ for(let i = 0; i <= 20; i++) {
     } else { 
         console.log(i + " is odd."); 
     }
+}
+
+
+
+// EXAMPLE: Break
+console.log("\nEXAMPLE: Break"); 
+
+console.log("Uncomment Code for Output"); 
+for (number = 1; number <= 12; number++) { 
+    if (number === 6) { 
+        console.log("I hate the number 6"); 
+        break; 
+    }
+
+    console.log("9 x " + number + " = " + (9 * number));  
+}
+
+
+
+// EXAMPLE: Arrays
+console.log("\nEXAMPLE: Favourite Foods Array"); 
+
+let foodArray = ["Cauliflower", "Curry", "Teriyaki Beef", "Butter Chicken", "Steak Salad"]
+
+console.log("Food Array: " + foodArray); 
+
+
+
+// EXAMPLE: Array Length
+console.log("\nEXAMPLE: Food Array Length");
+
+console.log("Food Array Length: " + foodArray.length); 
+
+
+
+// EXAMPLE: Array Items
+console.log("\nEXAMPLE: Food Array Length");
+
+console.log("Food Array Third Item: " + foodArray[2]); 
+
+
+
+// EXAMPLE: Array Mutation
+console.log("\nEXAMPLE: Food Array Mutation");
+
+foodArray[2] = "Asparagus"; 
+console.log("Modified Food Array: " + foodArray); 
+
+
+
+// EXAMPLE: Array Append
+console.log("\nEXAMPLE: Food Array Addition");
+
+foodArray.push("Bacon"); 
+console.log("Lengthened Array: " + foodArray); 
+
+
+
+// EXAMPLE: Array Iteration
+console.log("\nEXAMPLE: Food Array Iteration");
+
+for (let i = 0; i < foodArray.length; i++) { 
+    console.log(foodArray[i]); 
 }
