5b3f7b483576ed166cd1862e26dc2b863ea4176f
Alexander Madsen <akmadsen522@gmail.com>
2018-10-10 09:17:17 -0600

    Finished loop logic example

--- day-2/app.js
+++ day-2/app.js
@@ -27,3 +27,23 @@ console.log("\nEXAMPLE: For Times Tables");
 for (number = 1; number <= 12; number++) { 
     console.log("9 x " + number + " = " + (9 * number)); 
 }
+
+// Also disabled for being kind of long output
+// console.log("\n\n")
+// for (number = 1; number <= 12; number++) { 
+//     for(counter = 1; counter <= 12; counter++) {
+//         console.log(counter + " x " + number + " = " + (counter * number)); 
+//     }
+// }
+
+
+// EXAMPLE: Loop Logic
+console.log("\nEXAMPLE: Loop Logic"); 
+
+for(let i = 0; i <= 20; i++) { 
+    if(i % 2 === 0) { 
+        console.log(i + " is even."); 
+    } else { 
+        console.log(i + " is odd."); 
+    }
+}
