69349b03fcffe30a52fba2f852291e938c3705c2
Alexander Madsen <akmadsen522@gmail.com>
2018-10-11 09:02:20 -0600

    Put in screen exercisE

---
+++ day-3/examples/screen/script.js
@@ -0,0 +1,5 @@
+console.log("EXAMPLE: Screen"); 
+console.log("HEIGHT: " + screen.height); 
+console.log("WIDTH: " + screen.width); 
+console.log("COLOUR DEPTH: " + screen.colorDepth); 
+console.log("ORIENTATION: " + screen.orientation); 
