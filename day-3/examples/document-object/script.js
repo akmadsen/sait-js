89b4d70490e8ea44162f26acd0181c993a40e0c7
Alexander Madsen <akmadsen522@gmail.com>
2018-10-11 10:52:46 -0600

    Started modifying attributes

--- day-3/examples/document-object/script.js
+++ day-3/examples/document-object/script.js
@@ -17,7 +17,28 @@ console.log("\n\nEXAMPLE: Elements by Selector All");
 listElements = document.querySelectorAll("p"); 
 console.log(listElements); 
 
+listElements = document.querySelectorAll(".nav-item"); 
+console.log(listElements); 
+
 
 console.log("\n\nEXAMPLE: First Element");
 let firstElement = document.querySelector('p'); 
 console.log(firstElement); 
+
+firstElement = document.querySelector('#header'); 
+console.log(firstElement); 
+
+
+console.log("\n\nEXAMPLE: Collection Notation");
+listElements = document.querySelectorAll('p'); 
+console.log(listElements[1]); 
+console.log(listElements[3]); 
+
+
+console.log("\n\nEXAMPLE: Modifying Attributes");
+let image = document.querySelector('img'); 
+image.src = "http://www.placepuppy.net/500/600?random"; 
+console.log(image); 
+
+image.setAttribute('src', 'https://placekitten.com/500/600?random'); 
+console.log(image); 
