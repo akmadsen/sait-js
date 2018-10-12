63032b9fba5e3037cb6172d2b3af89013c2a7a85
Alexander Madsen <akmadsen522@gmail.com>
2018-10-12 09:45:03 -0600

    Finished experiments with interval and timer

--- day-4/app.js
+++ day-4/app.js
@@ -1,5 +1,39 @@
-let square = document.getElementById("box"); 
+let square = document.getElementById("box");
 
-square.style.position = "absolute"; 
-square.style.top = "100px"; 
-square.style.left = "100px"; 
+square.style.position = "absolute";
+square.style.top = "100px";
+square.style.left = "100px";
+
+
+// Timer Example 
+function doThisLater() {
+    alert("Time's Up!");
+}
+
+// // TIEN'S IDEA 
+// function laughter() { 
+//     document.write("ha "); 
+//     setTimeout(laughter, 500); 
+//     setTimeout(laughter, 500); 
+// }
+// setTimeout(laughter, 500); 
+
+let timerId = setTimeout(doThisLater, 3000); 
+clearTimeout(timerId); 
+
+// Interval Example
+
+let timeBox = document.getElementById('time'); 
+timeBox.innerHTML = new Date(); 
+
+// timeBox.style.position = "absolute"; 
+// timeBox.style.border = "2px solid black"; 
+// timeBox.style.top = "400px"; 
+// timeBox.style.right = "100px"; 
+// timeBox.style.padding = "10px";
+
+function updateTime() { 
+    timeBox.innerHTML = new Date(); 
+}
+
+let intervalTimer = setInterval(updateTime, 1000); 
