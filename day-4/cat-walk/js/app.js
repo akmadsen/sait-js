68a1f74a08a767958ace1b5dd104a8d77fd8047a
Alexander Madsen <akmadsen522@gmail.com>
2018-10-12 10:22:34 -0600

    Cat now has better range of motion

--- day-4/cat-walk/js/app.js
+++ day-4/cat-walk/js/app.js
@@ -1,19 +1,38 @@
-let walkForwards = true;
 let direction = 10;
+let vertDirection = 10; 
 let units = "px";
 
+// Rotation Stats
+let rot = 0; 
+let rotStep = 5; 
+let rotUnit = "deg"; 
+
 let img = document.getElementById('cat');
+
+// Initialize Values
 let currentLeft = parseInt(img.style.left);
+let currentTop = parseInt(img.style.top); 
 
 function catWalk() {
     currentLeft = parseInt(img.style.left);
     let leftBound = window.innerWidth - img.width; 
-    
+  
+    currentTop = parseInt(img.style.top); 
+    let topBound = window.innerHeight - img.height; 
+
     if ((currentLeft > leftBound) || (currentLeft < 0)) {
         direction *= -1;
     }
 
+    if ((currentTop > topBound) || (currentTop < 0)) { 
+        vertDirection *= -1; 
+    }
+
     img.style.left = (currentLeft + direction + units);
+    img.style.top = (currentTop + vertDirection + units);
+    
+    img.style.transform = "rotate(" + rot + rotUnit + ")"; 
+    rot = (rot + rotStep) % 360; 
 }
 
 setInterval(catWalk, 50);
