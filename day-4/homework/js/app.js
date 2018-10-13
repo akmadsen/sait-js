241e97cc085a2ebad17ff2b9a84a1f13c6fc2126
Alex Madsen <akmadsen522@gmail.com>
2018-10-13 14:46:08 -0600

    Now have the content of main and footer set up

--- day-4/homework/js/app.js
+++ day-4/homework/js/app.js
@@ -1,68 +1,67 @@
 // Data to work with 
-let gibsonQuotes = [
-    {
-        quote: "The future is here. It's just not widely distributed yet.", 
+let quotes = [{
+        quote: "The future is here. It's just not widely distributed yet.",
         author: "William Gibson"
-    }, 
+    },
     {
         quote: "Time moves in one direction, memory in another.",
         author: "William Gibson"
-    }, 
+    },
     {
         quote: "Language is to the mind more than light is to the eye.",
         author: "William Gibson"
-    }, 
+    },
     {
-        quote: "When you want to know how things really work, study them when they're coming apart.", 
+        quote: "When you want to know how things really work, study them when they're coming apart.",
         author: "William Gibson"
-    }, 
+    },
     {
-        quote: "Things aren't different. Things are things.", 
+        quote: "Things aren't different. Things are things.",
         author: "William Gibson"
     }
-]; 
+];
 
 // Components to work with 
-let quoteBox = document.getElementById('quote-box'); 
-let prevButton = document.getElementById('prev-button'); 
-let nextButton = document.getElementById('next-button'); 
+let quoteBox = document.getElementById('quote-box');
+let prevButton = document.getElementById('prev-button');
+let nextButton = document.getElementById('next-button');
 
 
 // Defined variables and constants
 let timeDelay = 3000; // End Result 30 Seconds 
-let currentIndex = 0; 
+let currentIndex = 0;
 
-function setQuote(index) { 
-    quoteBox.innerHTML = "<p>" + gibsonQuotes[index].quote + " - " + gibsonQuotes[index].author + "</p>"; 
+function setQuote(index) {
+    quoteBox.innerHTML = "<p>" + quotes[index].quote + " - " + quotes[index].author + "</p>";
 }
 
 // Quote Update Functions 
-function updateQuote() { 
-    currentIndex = (currentIndex + 1) % gibsonQuotes.length; 
-    setQuote(currentIndex); 
+function updateQuote() {
+    currentIndex = (currentIndex + 1) % quotes.length;
+    setQuote(currentIndex);
 }
 
 // Get Quote behaviour running 
-setQuote(currentIndex); 
-let timerId = setInterval(updateQuote, timeDelay); 
+setQuote(currentIndex);
+let timerId = setInterval(updateQuote, timeDelay);
 
-function resetTimer() { 
-    clearInterval(timerId); 
-    timerId = setInterval(updateQuote, timeDelay); 
+function resetTimer() {
+    clearInterval(timerId);
+    timerId = setInterval(updateQuote, timeDelay);
 }
 
-function nextQuote(event) { 
-    currentIndex = (currentIndex + 1) % gibsonQuotes.length; 
-    setQuote(currentIndex); 
-    resetTimer(); 
+function nextQuote(event) {
+    currentIndex = (currentIndex + 1) % quotes.length;
+    setQuote(currentIndex);
+    resetTimer();
 }
 
-function prevQuote(event) { 
-    currentIndex = (currentIndex - 1) >= 0 ? currentIndex - 1 : gibsonQuotes.length - 1; 
-    setQuote(currentIndex); 
-    resetTimer(); 
+function prevQuote(event) {
+    currentIndex = (currentIndex - 1) >= 0 ? currentIndex - 1 : quotes.length - 1;
+    setQuote(currentIndex);
+    resetTimer();
 }
 
 // Initialize Buttons 
-prevButton.addEventListener('click', prevQuote); 
-nextButton.addEventListener('click', nextQuote); 
+prevButton.addEventListener('click', prevQuote);
+nextButton.addEventListener('click', nextQuote);
