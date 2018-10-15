534ae4434b02547b94115878f937e40c7d1b9b01
Alexander Madsen <akmadsen522@gmail.com>
2018-10-15 14:39:28 -0600

    All but the "checkbox" questions are required... turns out checkboxes need a custom js function.

--- day-5/homework/js/app.js
+++ day-5/homework/js/app.js
@@ -1,1 +1,14 @@
-console.log("QUIZ CODE HOOKED UP"); 
+// Gather the answers from the form. 
+let plutoAnswer = document.quiz['pluto']; 
+let quiz = document.quiz; 
+
+let submitButton = document.getElementById('submit-button'); 
+
+submitButton.addEventListener('click', function(event) { 
+  
+    quiz.checkValidity(); 
+    quiz.reportValidity(); 
+
+    event.preventDefault(); 
+    console.log("We got here"); 
+});
