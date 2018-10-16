6055bee5b40e395e6cb7dc042e5fe45e53477eb8
Alexander Madsen <akmadsen522@gmail.com>
2018-10-16 10:18:12 -0600

    Document ready added (for example)

--- day-6/jquery/js/app.js
+++ day-6/jquery/js/app.js
@@ -1,33 +1,42 @@
-// Backup 
+// Backup (if CDN is down, use local)
 window.jQuery || document.write('<script src="js/jquery.min.js"><\/script>'); 
 
-// Selecting 
-let links = $('a'); 
-console.log(links);
+// Wait for the DOM to be loaded (not an all the time thing - can bog down pages)
+$(document).ready(function() {
+// $(function() { // Alt form
 
-// Changing CSS
-let google = $('#google a'); 
-let yahoo = $('#yahoo').find('a'); 
-
-
-google.html('Microsoft Bing');
-google.attr('href', 'https://www.bing.com'); 
-google.css('color', 'green'); 
-
-yahoo.html('Duck Duck Go'); 
-yahoo.attr('href', 'https://www.duckduckgo.com'); 
-yahoo.css({
-    'color': 'orange',
-    'background-color': 'rebeccapurple'
-}); 
-
-// Looping with JQuery 
-links.each(function() { 
-    let link = $(this); 
-
-    link.text(link.text() + "!"); 
-    link.css({
-        "background-color": "yellowgreen", 
-        "text-decoration": "none" 
+    // Selecting 
+    let links = $('a'); 
+    console.log(links);
+    
+    // Changing CSS
+    let google = $('#google a'); 
+    let yahoo = $('#yahoo').find('a'); 
+    
+    
+    google.html('Microsoft Bing');
+    google.attr('href', 'https://www.bing.com'); 
+    google.css('color', 'green'); 
+    
+    yahoo.html('Duck Duck Go'); 
+    yahoo.attr('href', 'https://www.duckduckgo.com'); 
+    yahoo.css({
+        'color': 'orange',
+        'background-color': 'rebeccapurple'
     }); 
+    
+    // Looping with JQuery 
+    let navLinks = $('nav a'); 
+    
+    navLinks.each(function() { 
+        let link = $(this); 
+        
+        link.text(link.text() + "!"); 
+        link.attr('href', 'http://worstsearch.com/'); 
+        link.css({
+            "background-color": "yellowgreen", 
+            "text-decoration": "none" 
+        }); 
+    }); 
+
 }); 
