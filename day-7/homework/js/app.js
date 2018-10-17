72063ab0da352164499b8c5f80de0022a57224c5
Alexander Madsen <akmadsen522@gmail.com>
2018-10-17 13:33:59 -0600

    Forms now have some custom error checking

--- day-7/homework/js/app.js
+++ day-7/homework/js/app.js
@@ -1,13 +1,55 @@
-fetch("https://api.lyrics.ovh/v1/Coldplay/Adventure of a Lifetime")
-.then(function(result) { 
-    return result.json(); 
-})
-.then(function(data) {
-    let container = document.getElementById('lyric-container'); 
-    let lyrics = document.createElement('div'); 
+// Defined Constants 
+const SUBMIT_BUTTON = document.getElementById("submit-button"); 
+const FORM = document.forms[0]; // We know there's only one form 
 
-    lyrics.innerHTML = data.lyrics.replace(/(\n|\r\n|\r)/g, "\<br \/\>"); 
+// Variables we Care About 
+let artist = "", song = ""; 
 
-    container.innerHTML = ""; // Clearing inner html
-    container.appendChild(lyrics); 
+// Function Definitions
+function fetchLyrics(artist, songTitle) {
+
+    fetch("https://api.lyrics.ovh/v1/" + artist + "/" + songTitle + "")
+        .then(function (result) {
+            if (!result.ok) {
+                throw Error(result.statusText);
+            }
+
+            return result.json();
+        })
+        .then(function (data) {
+            let container = document.getElementById('lyric-container');
+            let lyrics = document.createElement('div');
+
+            lyrics.innerHTML = data.lyrics.replace(/(\n|\r\n|\r)/g, "\<br \/\>");
+
+            container.innerHTML = ""; // Clearing inner html
+            container.appendChild(lyrics);
+        })
+        .catch(function (error) {
+            console.log("Error!");
+            console.log(error);
+        });
+}
+
+// Interactivity 
+
+SUBMIT_BUTTON.addEventListener('click', function(event) { 
+    event.preventDefault(); 
+
+    artist = FORM['artist'].value; 
+    song = FORM['song'].value; 
+
+    let errors = []; 
+
+    if(!artist) { 
+        errors.push("Artist must be defined"); 
+    }
+
+    if(!song) { 
+        errors.push("Song must be defined"); 
+    }
+
+    console.log(errors); 
+
 }); 
+// fetchLyrics("Celldweller", "Against the Tide");
