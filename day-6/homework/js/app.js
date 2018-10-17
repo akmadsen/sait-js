920e9009761d707e21c3c08b68683d6731254053
Alex Madsen <akmadsen522@gmail.com>
2018-10-16 18:26:13 -0600

    Getting some musculature in place

---
+++ day-6/homework/js/app.js
@@ -0,0 +1,9 @@
+// Safety check - make sure we have jQuery loaded in one form or another
+window.jQuery || document.write('<script src="./js/jquery.js"></script>');
+
+let hues = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink', 'monochrome']; 
+let luminosity = ['bright', 'light', 'dark']; 
+let count = 3; // Start with a default count of 3
+
+console.log(hues); 
+console.log(count); 
