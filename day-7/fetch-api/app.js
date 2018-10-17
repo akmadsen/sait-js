6d5295cde045062eda6d58672bb60c493718eb09
Alexander Madsen <akmadsen522@gmail.com>
2018-10-17 11:23:41 -0600

    Tinkered with sending data and errors

--- day-7/fetch-api/app.js
+++ day-7/fetch-api/app.js
@@ -1,13 +1,56 @@
 fetch("https://jsonplaceholder.typicode.com/users")
-.then(function(response) {
-    // Data is not actually returned 
-    // the json function will return parsed, usable json
-    // but this will be another promise
-    return response.json(); 
-}).then(function(data) { 
-    console.log(data); 
+    .then(function (response) {
+        // Data is not actually returned 
+        // the json function will return parsed, usable json
+        // but this will be another promise
+        return response.json();
+    }).then(function (data) {
+        console.log(data);
 
-    for(let i = 0; i < data.length; i++) { 
-        console.log(data[i].name + " + " + data[i].username); 
-    }
-}); 
+        for (let i = 0; i < data.length; i++) {
+            console.log(data[i].name + " + " + data[i].username);
+        }
+    });
+
+// Using Fetch API to send data 
+// 1) Create content you want to send 
+let user = {
+    username: "akmadsen",
+    id: 505167
+}
+
+// 2) Fetch the URL 
+fetch("https://jsonplaceholder.typicode.com/users/", {
+        // Set method 
+        method: "POST",
+
+        // Set headers 
+        headers: {
+            "Content-Type": "application/json"
+        },
+
+        // Set data 
+        body: JSON.stringify(user)
+    })
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+
+// Error Handling with Fetch API
+fetch("https://jsonplaceholder.typicode.com/404")
+    .then(function (response) {
+        if(!response.ok) { 
+            throw Error(response.status); 
+        }
+
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    })
+    .catch(function (error) {
+        console.log("The Error:", error); 
+    });
