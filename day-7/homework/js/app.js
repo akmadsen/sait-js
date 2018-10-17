20c15d161ea6fdb458f225d291c48ac2320cfeec
Alexander Madsen <akmadsen522@gmail.com>
2018-10-17 14:23:53 -0600

    Form now has style and function

--- day-7/homework/js/app.js
+++ day-7/homework/js/app.js
@@ -1,55 +1,100 @@
 // Defined Constants 
-const SUBMIT_BUTTON = document.getElementById("submit-button"); 
+
 const FORM = document.forms[0]; // We know there's only one form 
 
+const SUBMIT_BUTTON = document.getElementById("submit-button");
+const ERROR_MESSAGE_BOX = document.getElementById("error-messages"); 
+
 // Variables we Care About 
-let artist = "", song = ""; 
 
 // Function Definitions
-function fetchLyrics(artist, songTitle) {
+function displayLyrics(artist, songTitle, lyrics) {
+    console.log("TODO: Display Lyrics Nicely"); 
+    let container = document.getElementById('lyric-container');
+    let lyricsElement = document.createElement('div');
 
+
+    lyricsElement.innerHTML = lyrics;
+    container.innerHTML = ""; // Clearing inner html
+    container.appendChild(lyricsElement);
+}
+
+function displayLyricsNotFound() {
+    console.log("TODO: Handle Lyrics Not Found"); 
+}
+
+function displayErrors(errors) { 
+    // We know if we made it here, our errors array has at least one item 
+    let html = ""; 
+
+    html += "<p>Please fix the following errors:</p>"; 
+    html += "<ul>"; 
+
+    for(let i = 0; i < errors.length; i++) { 
+        html += "<li>"; 
+        html += errors[i]; 
+        html += "</li>"; 
+    }
+    
+    html += "</ul>"; 
+    ERROR_MESSAGE_BOX.innerHTML = html; 
+
+    if(ERROR_MESSAGE_BOX.classList.contains('hidden')) { 
+        ERROR_MESSAGE_BOX.classList.remove('hidden');
+    }
+}
+
+function clearErrors() {
+    ERROR_MESSAGE_BOX.innerHTML = ""; // Clear the error messages 
+    if (!ERROR_MESSAGE_BOX.classList.contains('hidden')) { 
+        ERROR_MESSAGE_BOX.classList.add("hidden"); 
+    }
+}
+
+function fetchLyrics(artist, songTitle) {
     fetch("https://api.lyrics.ovh/v1/" + artist + "/" + songTitle + "")
         .then(function (result) {
             if (!result.ok) {
-                throw Error(result.statusText);
+                throw Error(result.status);
             }
 
             return result.json();
         })
         .then(function (data) {
-            let container = document.getElementById('lyric-container');
-            let lyrics = document.createElement('div');
-
-            lyrics.innerHTML = data.lyrics.replace(/(\n|\r\n|\r)/g, "\<br \/\>");
-
-            container.innerHTML = ""; // Clearing inner html
-            container.appendChild(lyrics);
+            displayLyrics(artist, songTitle, data.lyrics);
         })
         .catch(function (error) {
-            console.log("Error!");
-            console.log(error);
+            console.log("CAUGHT ERROR: " + error); 
+            if (error.toString() === "404") { 
+                displayLyricsNotFound(); 
+            }
         });
 }
 
 // Interactivity 
 
-SUBMIT_BUTTON.addEventListener('click', function(event) { 
-    event.preventDefault(); 
+SUBMIT_BUTTON.addEventListener('click', function (event) {
+    event.preventDefault();
 
-    artist = FORM['artist'].value; 
-    song = FORM['song'].value; 
+    let artist = FORM['artist'].value;
+    let song = FORM['song'].value;
 
-    let errors = []; 
+    let errors = [];
 
-    if(!artist) { 
-        errors.push("Artist must be defined"); 
+    if (!artist) {
+        errors.push("Artist must be defined");
     }
 
-    if(!song) { 
-        errors.push("Song must be defined"); 
+    if (!song) {
+        errors.push("Song must be defined");
     }
 
-    console.log(errors); 
+    // If we have errors, stop dead 
+    if (errors.length !== 0) {
+        displayErrors(errors); 
+        return;
+    }
 
-}); 
-// fetchLyrics("Celldweller", "Against the Tide");
+    clearErrors(); 
+    fetchLyrics(artist, song);
+});
