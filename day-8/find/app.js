8a9b274221cf2ae423f5c9798d72798ebcf77755
Alexander Madsen <akmadsen522@gmail.com>
2018-10-18 11:27:21 -0600

    Find exercise done

---
+++ day-8/find/app.js
@@ -0,0 +1,23 @@
+// An array of posts
+const posts = [{
+        id: 23,
+        title: 'Becoming an ES6 Wizard',
+        body: 'Ille vivere. Ut ad te ...'
+    },
+    {
+        id: 29,
+        title: 'JavaScript Pro Tips',
+        body: 'Pellentesque euismod ...'
+    },
+    {
+        id: 31,
+        title: 'Acing your next JS interview',
+        body: 'Lorem ipsum dolor sit amet...'
+    }
+];
+
+// Find the post with ID 29
+const currentId = 29;
+const currentPost = posts.find(post => post.id === 29); 
+console.log(currentPost.title);
+console.log(currentPost.body);
