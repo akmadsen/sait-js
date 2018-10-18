5a35ba0b774c3c016639fd9e2c56a7a60e7c5a4a
Alexander Madsen <akmadsen522@gmail.com>
2018-10-18 10:35:29 -0600

    Finished template strings

--- day-8/template-strings/app.js
+++ day-8/template-strings/app.js
@@ -1,13 +1,16 @@
-const user = {
+const USER = {
     name: 'Cody',
     loginCount: 1,
     goldStatus: true
 };
-const className = 'container';
-const html = '<div class="' + className + '">' +
-    '<h2>Welcome' + (user.goldStatus ? ' to our gold status member, ' : ', ') +
-    user.name + '!' + '</h2>' +
-    '<p>Today is ' + new Date() + '</p>' +
-    '<p>You have logged in ' + ++user.loginCount + ' times.</p>' +
-    '</div>';
+
+const CLASS_NAME = 'container';
+const html = 
+    `<div class="${CLASS_NAME}">
+    <h2>Welcome${(USER.goldStatus ? ' to our gold status member, ' : ', ')}
+    ${USER.name}!</h2>
+    <p>Today is ${new Date()}</p>
+    <p>You have logged in ${++USER.loginCount} times.</p>
+    </div>`;
+    
 document.getElementById('target').innerHTML = html;
